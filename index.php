ea0e8895ce8e20e3118e773166fc17c0765c7b69
Wayne Calvin Warnke <warn4018@fredonia.edu>
2018-04-04 22:15:37 -0400

    master

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+// hotelManagement/index.php
+// Created: April 4th, 2018
+// Created by Wayne Warnke
+//
+// Last Edit: April 4th, 2018
+// Edited by Wayne Warnke
+
+
+// Hotel Management System - Login page
+//
+// index.php will serve as the login page for the system.
+// Guests, Managers/Employees will login from the same HTML form
+// on button press, system will fetch user information from DB and create
+// a session remembered throughout the system.
+//
+// Guest users will be taken to a guest page with guest options
+// Managers will be taken to a management page with management options
+// Employees will be taken to a employee page with options for employees
+//
+// A Description of the system will also be available on this page.
+// At button click, the description will be displayed using JS
+// At second button click, the description will be hidden.
+
+
+// TODO: Basic HTML
+// TODO: CSS Grid
+// TODO: Login Page
+// TODO: Product Description
+// TODO:
+
+?>
+
+<link rel="stylesheet" type="text/css" href="styles.css"/>
+
+<div class="grid-container">
+  <!-- Hotel Name -->
+    <div class="grid-item" id="hotelname-container">
+
+    </div><!-- end hotelname-container -->
+  <!-- Login -->
+    <div class="grid-item" id="login-container">
+
+    </div><!-- end login-container -->
+  <!-- Footer -->
+    <div class="grid-item" id="footer-container">
+
+    </div><!-- end footer-container -->
+
+</div><!-- end grid-container -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- END OF index.php -->
